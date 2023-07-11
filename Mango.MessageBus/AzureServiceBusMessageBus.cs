2dbd6b0954ab1f0d8110a2abd68da857b7cc6914
anilmicroservices <anilreddy2011@outlook.com>
2023-07-11 14:43:11 +0530

    Add project files.

---
+++ Mango.MessageBus/AzureServiceBusMessageBus.cs
@@ -0,0 +1,36 @@
+﻿
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.MessageBus
+{
+    public class AzureServiceBusMessageBus : IMessageBus
+    {
+        //can be improved
+        private string connectionString = "Endpoint=sb://mangorestaurantdemo2023.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=HtmPM1ZKwy7tds2ChmRAbT5dlpyPa1ORH+ASbFvcO5o=";
+
+        public async Task PublishMessage(BaseMessage message, string topicName)
+        {
+
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topicName);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+
+            await client.DisposeAsync();
+        }
+    }
+}
